1fea044048a0e288589a9f89edc2b7af3b353827
wanglei <296831181@qq.com>
2020-01-04 23:09:36 +0800

    网关鉴权过滤代码迁移

--- blade-gateway/src/main/java/org/springblade/gateway/filter/AuthFilter.java
+++ blade-gateway/src/main/java/org/springblade/gateway/filter/AuthFilter.java
@@ -21,14 +21,19 @@ import io.jsonwebtoken.Claims;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springblade.common.constant.CommonConstant;
+import org.springblade.common.constant.JgyhwConstant;
+import org.springblade.common.tool.MD5Util;
 import org.springblade.gateway.props.AuthProperties;
 import org.springblade.gateway.provider.AuthProvider;
 import org.springblade.gateway.provider.ResponseProvider;
 import org.springblade.gateway.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -37,6 +42,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 鉴权认证
@@ -50,6 +56,9 @@ public class AuthFilter implements GlobalFilter, Ordered {
 	private AuthProperties authProperties;
 	private ObjectMapper objectMapper;
 
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		String path = exchange.getRequest().getURI().getPath();
@@ -57,18 +66,47 @@ public class AuthFilter implements GlobalFilter, Ordered {
 			return chain.filter(exchange);
 		}
 		ServerHttpResponse resp = exchange.getResponse();
-		String headerToken = exchange.getRequest().getHeaders().getFirst(AuthProvider.AUTH_KEY);
-		String paramToken = exchange.getRequest().getQueryParams().getFirst(AuthProvider.AUTH_KEY);
-		if (StringUtils.isAllBlank(headerToken, paramToken)) {
-			return unAuth(resp, "缺失令牌,鉴权失败");
+		if(path.indexOf(JgyhwConstant.APPLICATION_MANAGE_NAME) != -1){
+			String headerToken = exchange.getRequest().getHeaders().getFirst(AuthProvider.AUTH_KEY);
+			String paramToken = exchange.getRequest().getQueryParams().getFirst(AuthProvider.AUTH_KEY);
+			if (StringUtils.isAllBlank(headerToken, paramToken)) {
+				return unAuth(resp, "缺失令牌,鉴权失败");
+			}
+			String auth = StringUtils.isBlank(headerToken) ? paramToken : headerToken;
+			String token = JwtUtil.getToken(auth);
+			Claims claims = JwtUtil.parseJWT(token);
+			if (claims == null) {
+				return unAuth(resp, "请求未授权");
+			}
+			return chain.filter(exchange);
+		}else{
+			// 验证权限
+			String nonce = exchange.getRequest().getHeaders().getFirst("nonce");
+			String sign = exchange.getRequest().getHeaders().getFirst("sign");
+			String timestamp = exchange.getRequest().getHeaders().getFirst("timestamp");
+			// 验证请求参数
+			if(StringUtils.isBlank(nonce) || StringUtils.isBlank(sign) || StringUtils.isBlank(timestamp)){
+				return unAuth(resp, "缺失令牌参数");
+			}
+			// 验证是否超时
+			Long timestampLong = Long.valueOf(timestamp);
+			int maxTimeout = 1000*60*5;
+			if(System.currentTimeMillis() - timestampLong < 0 || System.currentTimeMillis() - timestampLong > maxTimeout){
+				return unAuth(resp, "令牌过期");
+			}
+			// 验证随机值
+			if(stringRedisTemplate.hasKey(CommonConstant.AUTH_NONCE_KEY_PREFIX + nonce)){
+				return unAuth(resp, "重复请求");
+			}
+			// 验证签名
+			String md5 = MD5Util.stringToMD5(nonce + timestamp);
+			if(!sign.equals(md5)){
+				return unAuth(resp, "签名错误");
+			}
+			// 写入随机值到Redis
+			stringRedisTemplate.opsForValue().set(CommonConstant.AUTH_NONCE_KEY_PREFIX + nonce, nonce, maxTimeout, TimeUnit.MILLISECONDS);
+			return chain.filter(exchange);
 		}
-		String auth = StringUtils.isBlank(headerToken) ? paramToken : headerToken;
-		String token = JwtUtil.getToken(auth);
-		Claims claims = JwtUtil.parseJWT(token);
-		if (claims == null) {
-			return unAuth(resp, "请求未授权");
-		}
-		return chain.filter(exchange);
 	}
 
 	private boolean isSkip(String path) {
